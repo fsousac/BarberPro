07d8d758a37bcd178a7d510f4a9e32c4ad04cd20
Felipe de Sousa <felipescoelho517@gmail.com>
2025-08-13 12:54:36 +0000

    feat: iniciando comunicação com prisma e auth

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { PrismaMopd}
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, Auth],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async register(email: string, password: string, name: string) {
+    const hash = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: { email, passwordHash: hash, name },
+    });
+    return this.signToken(user.id, user.email);
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) throw new UnauthorizedException();
+    const valid = await bcrypt.compare(password, user.passwordHash);
+    if (!valid) throw new UnauthorizedException();
+    return this.signToken(user.id, user.email);
+  }
+
+  private signToken(userId: string, email: string) {
+    const payload = { sub: userId, email };
+    return { access_token: this.jwt.sign(payload) };
+  }
+}
